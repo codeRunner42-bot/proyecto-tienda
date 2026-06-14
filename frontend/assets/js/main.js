23419eaa7d50739c1d6d35137d141ff12790bf07
Jesus Aleman <ospajesu@gmail.com>
2026-06-14 14:24:06 -0500

    Cambio de número de contacto

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,3 +1,5 @@
+const PHONE = '573022880520'; // <--- CAMBIA ESTE NÚMERO (ejemplo: 573101234567)
+
 let products = [];
 let cart = JSON.parse(localStorage.getItem('cart')||'[]');
 const productsEl = document.getElementById('products');
